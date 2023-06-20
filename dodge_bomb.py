ae19cdbfe5e1677f9d931a26c67ad3c05f2c4bce
MakotoMiyake <c0b221424a@edu.teu.ac.jp>
2023-06-20 13:47:22 +0900

    練習問題２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -13,13 +13,18 @@ def main():
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
 
-    enn = pg.Surface((20, 20))
-    pg.draw.circle(enn, (255, 0, 0), (10,10), 10)
-    enn.set_colorkey((0, 0, 0))
-    loc = [random.randint(0, WIDTH-20), random.randint(0, HEIGHT-20)]
+    bomb_img = pg.Surface((20, 20))
+    pg.draw.circle(bomb_img, (255, 0, 0), (10,10), 10)
+    bomb_img.set_colorkey((0, 0, 0))
+    bomb_rect = bomb_img.get_rect()
+    bomb_rect.center = (
+        random.randint(0 + bomb_rect.width // 2, WIDTH - bomb_rect.width // 2),
+        random.randint(0 + bomb_rect.height // 2, HEIGHT - bomb_rect.height // 2)
+    )
 
     clock = pg.time.Clock()
     tmr = 0
+    vx = 5; vy = 5
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -27,9 +32,14 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+
+        # 爆弾の移動
+        bomb_rect.move_ip(vx, vy)
+        screen.blit(bomb_img, bomb_rect)
+
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
