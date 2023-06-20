d5ffa2cf65ab6e7dfdf466a3a782519e15e979a0
MakotoMiyake <c0b221424a@edu.teu.ac.jp>
2023-06-20 17:01:58 +0900

    追加機能３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,3 +1,4 @@
+import math
 import random
 import sys
 import pygame as pg
@@ -13,9 +14,7 @@ delta: dict = {  # 押下キーと移動量の対応辞書
     pg.K_RIGHT: (+5, 0)
 }
 
-accs = [a for a in range(1, 11)]  # 加速度リスト
-
-def chek_bound(obj: pg.Rect) -> tuple[bool, bool]:
+def check_bound(obj: pg.Rect) -> tuple[bool, bool]:
     """
     オブジェクトが画面内か画面外かを判定し、真偽値タプルを返す
     引数 obj: オブジェクト（爆弾、こうかとん）SurfaceのRectオブジェクト
@@ -39,8 +38,10 @@ def main():
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     kk_rect = kk_img.get_rect()
-    kk_rect.center = (900, 400)
-    kk_img_r = pg.transform.flip(kk_img, True, False)
+    kk_rect.center = (900, 400)  # 初期位置
+    kk_img_r = pg.transform.flip(kk_img, True, False)  # 反転こうかとん
+    kk_img_go = pg.image.load("ex02/fig/6.png")  # ゲームオーバーこうかとん
+    kk_img_go = pg.transform.rotozoom(kk_img_go, 0, 2.0)
 
     # 押下キーとrotozoomした画像の対応辞書
     kk_imgs: dict = {
@@ -76,6 +77,8 @@ def main():
     clock = pg.time.Clock()
     tmr = 0
     vx = 5; vy = 5  # 移動量
+    goflag = False  # ゲームオーバーフラグ
+    time = 0  # 経過時間
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -93,28 +96,42 @@ def main():
                 sum_mv[1] += mv[1]
         kk_rect.move_ip(sum_mv)
         # 更新後の位置が画面外になった場合、更新前の座標に戻す
-        if chek_bound(kk_rect) != (True, True):
+        if check_bound(kk_rect) != (True, True):
             kk_rect.move_ip([-sum_mv[0], -sum_mv[1]])
         
-        # 押下キーに応じたrotozoomした画像を描画
-        screen.blit(kk_imgs[tuple(sum_mv)], kk_rect)  # こうかとんの描画
-
         # 更新後の位置が画面外になった場合、移動量を反転する
-        width, height = chek_bound(bomb_rect)
+        width, height = check_bound(bomb_rect)
         if not width:  # 横方向に画面外だったら
             vx *= -1
         if not height:  # 縦方向に画面外だったら
             vy *= -1
         bomb_rect.move_ip(vx, vy)  # 爆弾の移動
-        # avx, avy = vx*accs[min(tmr//500, 9)], vy*accs[min(tmr//500, 9)]  # 加速度
-        # bomb_rect.move_ip(avx, avy)  # 爆弾の加速度移動
         bomb_img = bomb_imgs[min(tmr//500, 9)]  # 爆弾の拡大
         screen.blit(bomb_img, bomb_rect)  # 爆弾の描画
 
         # こうかとんと爆弾の衝突判定
         if kk_rect.colliderect(bomb_rect):
+            screen.blit(kk_img_go, kk_rect)  # ゲームオーバーこうかとんの描画
+            goflag = True
+        else:
+            screen.blit(kk_imgs[tuple(sum_mv)], kk_rect)  # こうかとんの描画
+        
+        if goflag:
             print("GAME OVER")
-            return 
+            screen.blit(kk_img_go, kk_rect)  # ゲームオーバーこうかとんの描画
+            screen.blit(font.render("Time: " + str(time), True, (0, 0, 255)), [0, 0])
+            screen.blit(font_go.render("GAME OVER", True, (255, 0, 0)), [430, 390])
+            pg.display.update()
+            clock.tick(0.5)
+            return
+        
+        if tmr % 50 == 0:
+            time += 1
+
+        # 経過時間の表示
+        font = pg.font.Font(None, 80) # 経過時間表示用のフォント
+        font_go = pg.font.Font(None, 160)  # ゲームオーバー時のフォント
+        screen.blit(font.render("Time: " + str(time), True, (0, 0, 255)), [0, 0])
 
         pg.display.update()
         tmr += 1
